fa7cbf8ab0a15763d69dccc9c339a4ce0db7a9ab
Common Vulnerabilities and Exploits Database <84289402+gitadvisor@users.noreply.github.com>
2023-05-20 14:04:56 -0700

    Create bus.go
    Signed-off-by: Common Vulnerabilities and Exploits Database <84289402+gitadvisor@users.noreply.github.com>

---
+++ bus.go
@@ -0,0 +1,13 @@
+package clio
+
+import (
+	"github.com/nextlinux/gopartybus"
+)
+
+type BusConstructor func(Config) *partybus.Bus
+
+var _ BusConstructor = newBus
+
+func newBus(_ Config) *partybus.Bus {
+	return partybus.NewBus()
+}
